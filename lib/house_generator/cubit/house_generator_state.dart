16ac53b0c70e0d8e40ba593bd41bbe96d934e67e
Andres Melenchon <andres.melenchon88@gmail.com>
2022-08-08 18:22:21 +0200

    Added Equatable to Cubit States

--- lib/house_generator/cubit/house_generator_state.dart
+++ lib/house_generator/cubit/house_generator_state.dart
@@ -1,19 +1,28 @@
 part of 'house_generator_cubit.dart';
 
-abstract class HouseGeneratorState {
+abstract class HouseGeneratorState extends Equatable {
   const HouseGeneratorState();
 }
 
 class GeneratorInitial extends HouseGeneratorState {
   const GeneratorInitial();
+
+  @override
+  List<Object?> get props => [];
 }
 
 class GeneratorLoading extends HouseGeneratorState {
   const GeneratorLoading();
+
+  @override
+  List<Object?> get props => [];
 }
 
 class GeneratorReceived extends HouseGeneratorState {
   final Attributes attributes;
 
-  GeneratorReceived(this.attributes);
+  const GeneratorReceived(this.attributes);
+
+  @override
+  List<Object?> get props => [attributes];
 }
